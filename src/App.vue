f75daec2caf1373406eeebf69e93a05e53c37d53
Cezar <cezaretci@gmail.com>
2022-10-30 09:50:39 +0200

    remove styles

--- src/App.vue
+++ src/App.vue
@@ -99,10 +99,6 @@
             display: flex;
             flex-direction: column;
             align-items: center;
-            background: #FFFFFF;
-            box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
-            border-radius: 4px;
-            gap: 1rem;
         }
     }
 
